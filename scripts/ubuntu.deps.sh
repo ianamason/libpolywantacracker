450a5e7a315c4c8d4d85ce63594086cc1e48de95
Ian A. Mason <iam@csl.sri.com>
2019-10-03 12:29:35 -0700

    Lets see how delicate this setup is...(1)

--- scripts/ubuntu.deps.sh
+++ scripts/ubuntu.deps.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-sudo apt-get install cmake make libgmp-dev python2.7-dev python-sympy
+sudo apt-get install cmake make libgmp-dev
+
+echo $PYTHON
+
+echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile;
+source ${HOME}/.bash_profile;
+
+pyenv global ${PYTHON};
+
+pyenv versions
+
+pip --version
+python --version
+
+pip install sympy
